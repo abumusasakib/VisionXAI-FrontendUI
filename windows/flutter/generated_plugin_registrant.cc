1d3c92642e933b71000faf8c5197edaff21cabe6
automation <automation@localhost>
2025-12-07 15:55:23 +0600

    fix: remove flutter_nsd dependency and update related plugin registrations; adjust version numbers for shared_preferences and package_info_plus

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,14 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <file_selector_windows/file_selector_windows.h>
-#include <flutter_nsd/flutter_nsd_plugin.h>
 #include <flutter_tts/flutter_tts_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
-  FlutterNsdPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterNsdPlugin"));
   FlutterTtsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterTtsPlugin"));
 }
