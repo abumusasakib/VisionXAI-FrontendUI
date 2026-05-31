4fbc350a35ac77012513916bf7c5dccb96b46640
abumusasakib <sakib3963@gmail.com>
2026-05-31 04:16:39 +0000

    Deploying to gh-pages from @ abumusasakib/VisionXAI-FrontendUI@2328ed3fbc9a8f08d9b56272b4074ddb07045b0a 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1907680562"
+    serviceWorkerVersion: "2461427676"
   }
 });
